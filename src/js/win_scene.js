259d29c5a25b810099498998f2713a2813c60556
algaba97 <jalgaba@ucm.es>
2017-01-10 12:14:54 +0100

    arreglado mapa2 bug y fallos menores cuando no hacia colision con death

---
+++ src/js/win_scene.js
@@ -0,0 +1,24 @@
+var win = {
+    create: function () {
+
+
+        //TODO 8 crear un boton con el texto 'Return Main Menu' que nos devuelva al menu del juego.
+        var goText = this.game.add.text(600, 100, "¡Has Ganado!");
+        var button2 = this.game.add.button(600, 350,
+                                          'button',
+                                          this.menuOnClick,
+                                          this, 2, 1, 0);
+        button2.anchor.set(0.5);
+        var texto2 = this.game.add.text(0, 0, "Main Menu");
+        texto2.anchor.set(0.5);
+        button2.addChild(texto2);
+
+    },
+
+    //TODO 7 declarar el callback del boton.
+  menuOnClick: function(){
+        this.game.state.start('menu');
+    }
+};
+
+module.exports = win;
